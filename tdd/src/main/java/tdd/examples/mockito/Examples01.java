d55208fe289726a15311503202aadd8555697981
kijungsong <skj4522@gmail.com>
2018-08-16 22:15:32 +0900

    Examples01 - 실행시 에러 발생 원인: 실제 호출은 add("two")를 했지만 verify는 add("one")을 함.

--- tdd/src/main/java/tdd/examples/mockito/Examples01.java
+++ tdd/src/main/java/tdd/examples/mockito/Examples01.java
@@ -3,14 +3,15 @@ package tdd.examples.mockito;
 import org.mockito.Mockito;
 
 import java.util.List;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mock; // Mockito의 static mock 메서드 import
 import static org.mockito.Mockito.verify;
 
 // Mocking과 Verification
 public class Examples01 {
     public static void main(String[] args){
         // mock
-        List mockedList = mock(List.class);
+        List mockedList = mock(List.class); // List를 구현하는 Mock 객체를 반환
+        // mockedList 레퍼런스가 참조하는 객체의 클래스 이름.
         System.out.println(mockedList.getClass().getName());
 
         // mock 사용하기
@@ -18,7 +19,7 @@ public class Examples01 {
         mockedList.clear();
 
         // verification
-        verify(mockedList).add("one");
-        verify(mockedList).clear();
+        verify(mockedList).add("one"); // mockedList.add("one")가 호출됬는지 체크.
+        verify(mockedList).clear(); // mockedList.clear();가 호출됬는지 체크
     }
 }
