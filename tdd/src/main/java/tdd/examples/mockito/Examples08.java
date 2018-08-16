a34a20a5a8dc59688976900280475946487fa6ea
kijungsong <skj4522@gmail.com>
2018-08-17 00:02:11 +0900

    Examples08 - @Mock, @RunWith(MockitoJUnitRunner.class)을 통한 Mock 객체 설정.

--- tdd/src/main/java/tdd/examples/mockito/Examples08.java
+++ tdd/src/main/java/tdd/examples/mockito/Examples08.java
@@ -7,6 +7,8 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.List;
 
+// @Mock 애노테이션과 @RunWith(MockitoJUnitRunner.class)을 통한 Mock객체 설정
+// @RunWith(MockitoJUnitRunner.class) 사용하면 Examples07의 MockitoAnnotations.initMocks를 호출안해도 Mock객체가 설정된다.
 @RunWith(MockitoJUnitRunner.class)
 public class Examples08 {
     @Mock
