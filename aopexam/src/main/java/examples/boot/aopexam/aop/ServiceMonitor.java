934b2d0d13fc14439c8c1948922567bc5fbcfd63
Kijung Song <skj4522@gmail.com>
2018-08-28 18:37:58 +0900

    [Aspect] 메서드 실행 전에 로그 출력

--- aopexam/src/main/java/examples/boot/aopexam/aop/ServiceMonitor.java
+++ aopexam/src/main/java/examples/boot/aopexam/aop/ServiceMonitor.java
@@ -4,13 +4,21 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
-@Aspect
+@Aspect // AOP를 위한 객체라는 것을 알림
 @Component
 public class ServiceMonitor {
-    @AfterReturning("execution(* examples..*Service.*(..))")
-    public void logServiceAccess(JoinPoint joinPoint) {
+    // 메소드가 실행될 때 메소드 이름 출력.
+    @Before("execution(* examples..*Service.*(..))")
+    public void logServiceBefore(JoinPoint joinPoint) {
+        System.out.println("name : " + joinPoint.getSignature().getName());
+        System.out.println("Before: " + joinPoint);
+    }
+
+    @AfterReturning("execution(* examples..*Service.*(..))") // 애노테이션이 Point Cut 역할.
+    public void logServiceAccess(JoinPoint joinPoint) { // 이 메서드가 Advice의 역할을 함. (파라미터로 Join Point를 받음)
         System.out.println("name : " + joinPoint.getSignature().getName());
         System.out.println("Completed: " + joinPoint);
     }
--- aopexam/src/main/java/examples/boot/aopexam/service/MemberServiceImpl.java
+++ aopexam/src/main/java/examples/boot/aopexam/service/MemberServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
+// 서비스의 메소드가 실행될 때마다 메소드의 이름을 출력하시오.
 @Service
 public class MemberServiceImpl implements MemberService{
     @Override
@@ -19,8 +20,7 @@ public class MemberServiceImpl implements MemberService{
         List<Member> list = new ArrayList<>();
         list.add(new Member(1L, "kim", 10));
         list.add(new Member(2L, "kang", 20));
-        if(1 == 1)
-            throw new RuntimeException("1 == 1");
+//        if(1 4hrow new RuntimeException("1 == 1");
         list.add(new Member(3L, "hong", 15));
         list.add(new Member(4L, "choi", 23));
         return list;
